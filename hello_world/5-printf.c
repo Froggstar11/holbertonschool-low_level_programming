5ccded1349e58ed5d2c5275d03eeede3df583fe2
miguel damasco <migueldamasco111@gmail.com>
2023-02-15 09:07:03 -0800

    5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char newText[] = "with proper grammar, but the outcome is a piece of art,";
+printf("%s\n",newText);
+return(0); 
+}  
