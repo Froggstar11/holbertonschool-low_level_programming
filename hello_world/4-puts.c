4429e2d03a2bf4e38e3b59480327845f847c0cb8
miguel damasco <migueldamasco111@gmail.com>
2023-02-15 08:10:57 -0800

    4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,10 @@
-char[] puts("Programming is like building a multilingual puzzle");
+#include<iostream>
+using namespace std;
+int main()
+{
+    char[] puts("Programming is like building a multilingual puzzle");
+    return 0;
+}
+
+
  
