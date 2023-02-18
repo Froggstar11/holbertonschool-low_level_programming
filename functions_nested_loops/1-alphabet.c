9692516be164c69a154cb649d1852a507b776f8b
miguel damasco <migueldamasco111@gmail.com>
2023-02-18 12:38:44 -0800

    1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet -print the alphabet
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+int i;
+for(i = 97; i <= 122; i++)
+_putchar(i);
+_putchar('\n');
+}
