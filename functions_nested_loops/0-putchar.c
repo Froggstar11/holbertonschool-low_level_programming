7a971ab6dbd6cf394839f10123de58f8504f6874
miguel damasco <migueldamasco111@gmail.com>
2023-02-18 12:25:49 -0800

    0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int characters[] = { 95, 112, 117, 116, 99, 104, 97, 114};
+int i;
+for(i = 0; i <= 7; i++)
+putchar(characters[i]);
+return (0);
+}
