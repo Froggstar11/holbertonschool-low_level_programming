e841f2b134864370de420f6f41f14ac0bb33c3e3
miguel damasco <migueldamasco111@gmail.com>
2023-02-18 11:52:03 -0800

    8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+int j;
+for (j = 48; j <= 57; j++)
+putchar(j);
+for (i = 97; i <= 102; i++)
+putchar(i); 
+putchar('\n');
+return (0);
+}
