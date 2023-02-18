d4b0e7c28eb12eecb9e27ef7b4ba378e2371fc80
miguel damasco <migueldamasco111@gmail.com>
2023-02-18 14:28:27 -0800

    6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - check the code.
+ * @n: an int.
+ * Return: absoulte number of n.
+ */
+int _abs(int n)
+{
+int absNumber = abs(n);
+return (absNumber);
+}
