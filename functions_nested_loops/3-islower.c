d58e23d354489e3fc6b49748ffda10df2e56b607
miguel damasco <migueldamasco111@gmail.com>
2023-02-18 13:39:09 -0800

    3

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -2,12 +2,12 @@
 
 /**
  * _islower - if the parameter c is lowercase returns 1 otherwise 0.
- *
+ * @c: is a character.
  * Return: 1 if c is lowercase otherwise 0.
  */
 int _islower(int c)
 {
-if ( c >= 97 && c <= 122)
+if (c >= 97 && c <= 122)
 return (1);
 return (0);
 }
