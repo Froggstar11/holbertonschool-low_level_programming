ed2389c7bbe2ec8cd1c1eb6effc4823c56f3bba1
miguel damasco <migueldamasco111@gmail.com>
2023-02-18 12:50:07 -0800

    2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print the alphabet 10 times.
+ *
+ * Return: void.
+ */
+void print_alphabet_x10(void)
+{
+int i;
+int j;
+for (i = 0; i <= 9; i++)
+{
+for(j = 97; j <= 122; j++)
+_putchar(j);
+_putchar('\n');
+}
+}
