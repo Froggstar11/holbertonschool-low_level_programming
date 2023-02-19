36d7ff8effa77365f9411ebfed2c61c3803f9cd7
miguel damasco <migueldamasco111@gmail.com>
2023-02-18 16:36:46 -0800

    10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - check the code.
+ * @Number1: an int.
+ * @Number2: an int.
+ * Return: Always 0.
+ */
+int add(int Number1, int Number2)
+{
+return (Number1 + Number2);
+}
