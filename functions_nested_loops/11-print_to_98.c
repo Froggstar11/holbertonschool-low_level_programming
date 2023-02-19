e47fc716f835de4f8bee3c9157527beeac5677d8
miguel damasco <migueldamasco111@gmail.com>
2023-02-18 17:32:45 -0800

    11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -14,7 +14,11 @@ void print_to_98(int n)
       for(i = n; i >= 98; i--)
 	{
 	  if (i != 98)
-	  printf("%d, ",i);
+	  {
+	  printf("%d",i);
+	  _putchar(',');
+	  _putchar(' ');
+	  }
 	  if(i == 98)
 	  printf("%d", i);  
   
