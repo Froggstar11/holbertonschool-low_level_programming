5859818f3d135b8c24fcf0b429dafbbd5ae740ff
miguel damasco <migueldamasco111@gmail.com>
2023-02-22 08:30:45 -0800

    8

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * fizz_buzz - check the code
+ *
+ * Return: void.
+ */
+void fizz_buzz(void)
+{
+  int i;
+  for (i = 0; i <= 100; i++)
+    {
+      if (i % 3 == 0)
+	printf("Fizz ");
+      else if (i % 5 == 0)
+	printf("Buzz ");
+      else if (i % 3 == 0 && i % 5 == 0)
+	printf("FizzBuzz ");
+    }
+}
