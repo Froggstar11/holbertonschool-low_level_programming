25259b1c0f6e2e49b4796fca95326a3f0d439523
miguel damasco <migueldamasco111@gmail.com>
2023-02-22 08:45:13 -0800

    9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -14,8 +14,9 @@ int main(void)
 	printf("Fizz ");
       else if (i % 5 == 0)
 	printf("Buzz ");
-      else if (i % 3 == 0 && i % 5 == 0)
+      else if (i % 3 == 0 || i % 5 == 0)
 	printf("FizzBuzz ");
+      else
       printf("%d ",i);
     }
   return (0);
