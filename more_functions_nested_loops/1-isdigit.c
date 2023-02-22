354559e80636695ecc101f2ad98eefe064e5f7b2
miguel damasco <migueldamasco111@gmail.com>
2023-02-22 05:39:38 -0800

    1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isdigit - check the code
+ * @c: an int
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+int i;
+for (i = 48; i <= 57; i++)
+{
+if( c == i)
+{
+return (1);
+}
+return (0);
+}
+}
