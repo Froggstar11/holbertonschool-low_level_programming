73a5983955e241e3eee48f608cec16f26aa99ae8
miguel damasco <migueldamasco111@gmail.com>
2023-02-22 06:30:19 -0800

    5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * more_numbers - check the code
+ *
+ * Return: void.
+ */
+void more_numbers(void)
+{
+int i, j;
+int array[2];
+for (i = 0; i <= 14; i++)
+{
+if (i > 9)
+{
+array[0] = (i / 10) + 48;
+array[1] = (i % 10) + 48;
+for (j = 0; j <= 1; j++)
+_putchar(array[j]);
+}
+else if (i < 9)
+{
+_putchar(i + 48);
+}
+}
+_putchar('\n');
+}
