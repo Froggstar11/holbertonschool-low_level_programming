3fbcd87d57237456eb3da429f15df96608dc5020
miguel damasco <migueldamasco111@gmail.com>
2023-02-22 10:34:33 -0800

    10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -6,24 +6,23 @@
  */
 void print_triangle(int size)
 {
-  int i, j;
-  int count = size;
-  if(size <= 0)
-    _putchar('\n');
-  else
-    {
-      for (i = 0; i < size; i++)
-	{
-	  for (j = 0; j < size; j++)
-	    {
-	      if ( j >= count-1)
-		_putchar('#');
-	      else
-		_putchar(32);
-	    }
-	  count--;
-	  _putchar('\n');
-	}
-    }
+int i, j;
+int count = size;
+if (size <= 0)
+_putchar('\n');
+else
+{
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+if ( j >= count-1)
+_putchar('#');
+else
+_putchar(32);
 }
-	      
+count--;
+_putchar('\n');
+}
+}
+}
