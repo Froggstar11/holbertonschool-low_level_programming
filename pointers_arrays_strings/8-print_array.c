6e6992e1d7e4dc309dc8cb419a3a77f62b63c9c9
miguel damasco <migueldamasco111@gmail.com>
2023-02-27 00:39:38 -0800

    8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -12,11 +12,14 @@ void print_array(int *a, int n)
 int i;
 for (i = 0; i < n; i++)
 {
-printf("%d", a[i]);
+printf("%d, ", a[i]);
 if (i != n - 1)
 {
-_putchar(',');
-_putchar(' ');
+printf("%d, ", a[i]);
+}
+else
+{
+printf("%d", a[i]);
 }
 }
 _putchar('\n');
