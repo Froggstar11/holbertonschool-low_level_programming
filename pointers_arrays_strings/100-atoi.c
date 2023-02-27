b8aa3bbf3a34f8d4d07ef35980aeb12006903e2a
miguel damasco <migueldamasco111@gmail.com>
2023-02-27 07:36:49 -0800

    10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -17,7 +17,7 @@ int _atoi(char *s)
 	{
 	  if (firstPosition == -1)
 	    {
-	      printf("check i: %d", s[i]);
+	      printf("check i: %c", s[i]);
 	  number *= 10;
 	  number += s[i] - '0';
 	  if(s[i+1] < 48 && s[i+1] > 57)
