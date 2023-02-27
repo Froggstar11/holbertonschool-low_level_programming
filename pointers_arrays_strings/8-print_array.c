8d3cb495b67effdd8c2874d18df8737c83300e7c
miguel damasco <migueldamasco111@gmail.com>
2023-02-27 00:34:02 -0800

    8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_array - prints n elements of an array of integers, followed by a new line.
+ * @n: an int.
+ * @a: a pointer of int type.
+ * Return: void.
+ */
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+_putchar(a[i]);
+if (i != n - 1)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
+}
