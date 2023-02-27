2652de10d57cbbda259034c16ba477913627c6f8
miguel damasco <migueldamasco111@gmail.com>
2023-02-27 01:00:08 -0800

    8

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * strcpy - copies a string.
+ * @dest: a pointer of char type.
+ * @src: a pointer of char type.
+ * Return: a char pointer.
+ */
+char *_strcpy(char *dest, char *src)
+{
+strcpy(*dest, *src);
+return (dest*);
+}
