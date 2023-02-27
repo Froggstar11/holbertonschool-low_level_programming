bfd35f96882bdf707f0394908da416d488e5ca32
miguel damasco <migueldamasco111@gmail.com>
2023-02-26 21:24:26 -0800

    4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <string.h>
 /**
  * print_rev - prints in revers a string.
  * @s: a pointer of char type.
@@ -8,7 +8,7 @@
 void print_rev(char *s)
 {
 int i;
-int size = lenstr(s);
+int size = strlen(s);
 for (i = size-1; i >= 0; i--)
 {
 _putchar(s[i]);
