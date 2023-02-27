1eebaf7e54339bfa50be16d65e83d76885e6ac24
miguel damasco <migueldamasco111@gmail.com>
2023-02-27 06:19:09 -0800

    8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -10,6 +10,8 @@
 void print_array(int *a, int n)
 {
 int i;
+if ( n > 0)
+{
 for (i = 0; i < n; i++)
 {
 if (i != (n - 1))
@@ -21,4 +23,8 @@ else if (i == (n - 1))
 printf("%d\n", a[i]);
 }
 }
+}
+else
+{
+printf(" \n");
 }
