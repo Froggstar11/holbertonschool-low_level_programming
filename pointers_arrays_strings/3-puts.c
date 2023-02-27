1b402b573de493b8b57c89b3a2a90b6e3962e5d8
miguel damasco <migueldamasco111@gmail.com>
2023-02-26 21:07:31 -0800

    2

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - print a tring followed by a new line.
+ * @s: a pointer of char type.
+ * Return: void.
+ */
+void _puts(char *str)
+{
+int i;
+for (i = 0; i <= strlen(s); i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
