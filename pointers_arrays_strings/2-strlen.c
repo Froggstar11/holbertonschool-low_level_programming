fe2efc2aaa595c8036917d53913ca9e62a2e94d5
miguel damasco <migueldamasco111@gmail.com>
2023-02-26 20:56:51 -0800

    2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen - see the length of a string.
+ * @s: a point of char type.
+ * Return: an int.
+ */
+int _strlen(char *s)
+{
+return (strlen(*s));
+}
