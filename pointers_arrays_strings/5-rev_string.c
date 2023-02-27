582cd9971ca644f6dc7070dee3efcad76b108795
miguel damasco <migueldamasco111@gmail.com>
2023-02-26 21:32:21 -0800

    5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - reverses a string.
+ * @s: a pointer of char type.
+ * Return: void.
+ */
+void rev_string(char *s)
+{
+int i;
+int size = strlen(s);
+int count = 0;
+for (i = size - 1; i >= 0; i--)
+{
+*s[count] = s[i];
+count++;
+}
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,3 +1,4 @@
+void rev_string(char *s);
 void _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
