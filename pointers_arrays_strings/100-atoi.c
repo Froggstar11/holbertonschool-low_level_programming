b611eff60ea0fc9994b57f898bcff18dc350faeb
miguel damasco <migueldamasco111@gmail.com>
2023-02-27 01:53:26 -0800

    8

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _atoi - convert a string to an integer.
+ * @s: a pointer of char type.
+ * Return: an int.
+ */
+int _atoi(char *s)
+{
+  int i, plus, maius, number;
+  int firstPosition = -1;
+  int size = strlen(s);
+  for (int i = 0; i < size; i++)
+    {
+      if (s[i] => 48 && s[i] <= 57)
+	{
+	  if (firstPosition == -1)
+	    {
+	      firstPosition = s[i];
+	    }
+	  number *= 10;
+	  number += s[i] - '0';
+	}
+      else
+	{
+	  if ( firstPosition == -1)
+	    {
+	      if (s[i] == '+')
+		plus++;
+	      else if (s[i] == '-')
+		minus++;
+	    }
+	}
+    }
+  if ( firstPosition == -1)
+    return (0);
+  else if( plus >= minus)
+    return (number);
+  else if ( minus > plus)
+    return (-1 * number);
+}
