8899f18aba51485418f3bbc24dcf6d9ebc1c174d
miguel damasco <migueldamasco111@gmail.com>
2023-02-28 07:41:26 -0800

    100

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -9,36 +9,35 @@
 
 int _atoi(char *s)
 {
-  int newNumber;
-  unsigned int i = 0;
-  unsigned int min = 0;
-  unsigned int number = 0;
-  unsigned int size = strlen(s);
-
-  for (i = 0; i < size; i++)
-    {
-      if(s[i] == '-')
-	{
-	  min++;
-	}
-      if (s[i] > 47 && s[i] < 58)
-	{
-	  break;
-	}
-    }
-  while (s[i] > 47 && s[i] < 58)
-    {
-      newNumber = (((int)s[i]) - '0'); 
-      number *= 10;
-      number += newNumber;
-      i++;
-    }
-  if (min % 2 == 0)
-    {
-      return (number);
-    }
-  else
-    {
-      return (-number);
-    }
+int newNumber;
+unsigned int i = 0;
+unsigned int min = 0;
+unsigned int number = 0;
+unsigned int size = strlen(s);
+for (i = 0; i < size; i++)
+{
+if(s[i] == '-')
+{
+min++;
+}
+if (s[i] > 47 && s[i] < 58)
+{
+break;
+}
+}
+while (s[i] > 47 && s[i] < 58)
+{
+newNumber = (((int)s[i]) - '0'); 
+number *= 10;
+number += newNumber;
+i++;
+}
+if (min % 2 == 0)
+{
+return (number);
+}
+else
+{
+return (-number);
+}
 }
