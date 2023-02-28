0703165bdf2da90ed15734e23ccbadb0af37395a
miguel damasco <migueldamasco111@gmail.com>
2023-02-28 09:53:31 -0800

    2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncpy - check the code
+ * @dest: a pointer of char type.
+ * @src: a point of char type.
+ * @n: an int.
+ * Return: a char.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+}
