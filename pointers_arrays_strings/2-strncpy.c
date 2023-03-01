73f013e2975093505b733969feba5db187bf16b0
miguel damasco <migueldamasco111@gmail.com>
2023-02-28 19:20:36 -0800

    2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -15,7 +15,7 @@ char *_strncpy(char *dest, char *src, int n)
 size_t i;
 int sizeDest = strlen(dest);
 size_t number = sizeof n;  	
-for (i = 0; i < number; i++)
+for (i = 0; i < number - 1; i++)
 {
 dest[sizeDest] = src[i];
 sizeDest++;
