15a2dc6718f6dadc8571c31ed00149c49f5438f8
miguel damasco <migueldamasco111@gmail.com>
2023-03-01 05:39:10 -0800

    6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -11,7 +11,8 @@ char *cap_string(char *s)
 int i, j;
 int convert;
 int size = strlen(s);
-int array[] = { ' ', '\t', '\n', ',' , ';', '.', '?', '"', '(', ')', '{', '}' }; 
+int array[] = { ' ', '\t', '\n', ',', ';', '.', '?',
+'"', '(', ')', '{', '}' };
 for (i = 0; i < size; i++)
 {
 if (i == 0 && s[i] >= 97 && s[i] <= 122)
@@ -23,10 +24,10 @@ else
 {
 for (j = 0; j < 12; j++)
 {
-if (s[i] == array[j] && s[i+1] >= 97 && s[i+1] <= 122)
+if (s[i] == array[j] && s[i + 1] >= 97 && s[i + 1] <= 122)
 {
-convert = s[i+1] - 32;
-s[i+1] = convert;
+convert = s[i + 1] - 32;
+s[i + 1] = convert;
 }
 }
 }
