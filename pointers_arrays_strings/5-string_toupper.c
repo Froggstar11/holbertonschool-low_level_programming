280261eb1977d16f4d600bb1e46dd680d01d798d
miguel damasco <migueldamasco111@gmail.com>
2023-03-01 00:07:02 -0800

    5

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -4,19 +4,20 @@
 /**
  * string_toupper - change all characters lowercase to uppercase of a string
  * @s: a pointer of char type.
- * Return: Always 0.
+ * Return: a char.
  */
-void *string_toupper(char *s)
+char *string_toupper(char *s)
 {
 int i;
 int convert;
 int size = _strlen(s);
 for ( i = 0; i < size; i++)
 {
-if(s[i] >= 97 && s[i] <= 122)
+if (s[i] >= 97 && s[i] <= 122)
 {
 convert = s[i] - 32;
 s[i] = convert;
 }
 }
+return (s);
 }
