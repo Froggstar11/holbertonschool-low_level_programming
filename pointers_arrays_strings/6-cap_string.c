cb4f6b5de52e08b51e67b563a04a4612b51a3a3e
miguel damasco <migueldamasco111@gmail.com>
2023-03-01 00:34:58 -0800

    5

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * cap_string - capitalizes all words of a string.
+ * @s: a pointer of char type.
+ * Return: a char.
+ */
+char *cap_string(char *s)
+{
+int i, j;
+int convert;
+int size = strlen(s);
+int importantArray[] = {' ', '\t', '\n', ',', ';', '.', '?', '"', '(', ')', '{', '{'};
+for (i = 0; i < size; i++)
+{
+for ( j = 0; j < 12; j++)
+{
+if (s[i] == importantArray[j])
+{
+if ( i < size - 1)
+{
+convert = s[i+1] - 32;
+s[i+1] = convert;
+}
+else
+{
+continue;
+}
+}
+}
+}
+return (s);
+}
