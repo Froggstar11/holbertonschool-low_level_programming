f26f275570c62d410cb919d7cbae4b03f0d7ed86
miguel damasco <migueldamasco111@gmail.com>
2023-03-01 07:29:15 -0800

    0

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _memset - prints buffer in hexa
+ * @s: a pointer of char type
+ * @b: a char.
+ * @n: an unsigned int.
+ *
+ * Return: Nothing.
+ */
+char _memset(char *s, char b; unsigned int n)
+{
+  int i;
+  for (i = 0, i < n; i++)
+    {
+      s[i] = b;
+    }
+}
