0b28845e81508a3e694582562397f2503607d705
miguel damasco <migueldamasco111@gmail.com>
2023-02-28 22:45:41 -0800

    3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -29,6 +29,7 @@ int _strcmp(char *s1, char *s2)
 	{
 	  if ((s2[i] >= 97 && s2[i] <= 122) || (s2[i] >= 65 && s1[i] <= 90))
 	    {
+	      printf("llega");
 	      compare1 = s1[i];
 	      compare2 = s2[i];
 	      break;
