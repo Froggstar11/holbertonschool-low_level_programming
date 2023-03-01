5a0094d0a0bebb98b3882af7f4611176263fbcd6
miguel damasco <migueldamasco111@gmail.com>
2023-03-01 02:50:37 -0800

    7

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -8,20 +8,19 @@
  */
 char *leet(char *s)
 {
-  int i, j;
-  int size = strlen(s);
-  char array1[] = { 'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
-  int array2[] = { 4, 4, 3, 3, 0, 0, 7, 7, 1, 1};
-  for (i = 0; i < size; i++)
-    {
-      for (j = 0; j < 10; j++)
-	{
-	  if( s[i] == array1[j])
-	    {
-	      s[i] = array2[j] + '0';
-	    }
-	}
-	
-    }
-  return (s);
+int i, j;
+int size = strlen(s);
+char array1[] = { 'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+int array2[] = { 4, 4, 3, 3, 0, 0, 7, 7, 1, 1};
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < 10; j++)
+{
+if( s[i] == array1[j])
+{
+s[i] = array2[j] + '0';
+}
+}
+}
+return (s);
 }
