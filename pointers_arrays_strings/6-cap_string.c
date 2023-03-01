bd981dba4d59efdba91892bda89f7c3834340761
miguel damasco <migueldamasco111@gmail.com>
2023-03-01 05:36:27 -0800

    6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -14,22 +14,22 @@ int size = strlen(s);
 int array[] = { ' ', '\t', '\n', ',' , ';', '.', '?', '"', '(', ')', '{', '}' }; 
 for (i = 0; i < size; i++)
 {
-  if (i == 0 && s[i] >= 97 && s[i] <= 122)
-    {
-      convert = s[i] - 32;
-      s[i] = convert;
-    }
-  else
-    {
-      for (j = 0; j < 12; j++)
-	{
-	  if (s[i] == array[j] && s[i+1] >= 97 && s[i+1] <= 122)
-	    {
-	      convert = s[i+1] - 32;
-	      s[i+1] = convert;
-	    }
-	}
-    }
+if (i == 0 && s[i] >= 97 && s[i] <= 122)
+{
+convert = s[i] - 32;
+s[i] = convert;
+}
+else
+{
+for (j = 0; j < 12; j++)
+{
+if (s[i] == array[j] && s[i+1] >= 97 && s[i+1] <= 122)
+{
+convert = s[i+1] - 32;
+s[i+1] = convert;
+}
+}
+}
 }
 return (s);
 }
