1fa95390b78f4f6bdf6d1bd89ac4533efe819264
miguel damasco <migueldamasco111@gmail.com>
2023-03-02 05:39:39 -0800

    3

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -9,20 +9,33 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-  int i, j;
+  int i, j, z, x, d;
   unsigned int count = 0;
   int sizeS = strlen(s);
+  int aparition[100];
   int sizeAccept = strlen(accept);
+  for (z = 0; z < 100; z++)
+    {
+      for (x = 0; x < sizeS; x++)
+	{
+	  if ( aparition[z] != s[x]);
+	  aparition[z] = s[x];
+	}
+    }
   for (i = 0; i < sizeS; i++)
     {
       for (j = 0; j < sizeAccept; j++)
 	{
 	  if (s[i] == accept[j])
 	    {
-	      count++;
+	      for ( d = 0; d < 100; d++)
+		{
+		  if (s[i] != aparition[d])
+		    count++;
+		}
 	    }
 	}
     }
   return (count);
 }
-	    
+	   
