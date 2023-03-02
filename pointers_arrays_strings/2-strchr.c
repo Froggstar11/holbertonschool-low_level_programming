c1ab62c7dc466f5956ebb9644ce6727f3a741e94
miguel damasco <migueldamasco111@gmail.com>
2023-03-02 01:58:19 -0800

    2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -10,14 +10,13 @@
 char *_strchr(char *s, char c)
 {
   int i;
-  char p = NULL;
   int size = strlen(s);
   for (i = 0; i < size; i++)
     {
       if (s[i] == c)
 	{
-	p = s[i];
+	  break;
 	}
     }
-  return (p);
+  return (s[i]);
 }
