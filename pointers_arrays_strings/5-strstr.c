bdeca6d6ec5378d5eb39176fcb54ac0dace8491c
miguel damasco <migueldamasco111@gmail.com>
2023-03-03 00:31:45 -0800

    5

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -34,7 +34,7 @@ char *_strstr(char *haystack, char *needle)
       if (exist != 1)
 	count = 0;
       if (count == sizeNeedle)
-	p = &haystack[i - needleSize];
+	p = &haystack[i - sizeNeedle];
       exist = 0;
     }
   return (p);
