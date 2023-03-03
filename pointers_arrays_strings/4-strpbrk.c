97623d674b4171c94ef03fd285278a79bfad8075
miguel damasco <migueldamasco111@gmail.com>
2023-03-02 23:17:53 -0800

    4

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strpbrk - search a string for any of set of bytes.
+ * @s: a pointer of char type.
+ * @accept: a pointer of char type.
+ * Return: a pointer of char type.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+int i, j;
+int sizeS = strlen(s);
+int sizeAccept = strlen(accept);
+char *p;
+for (i = 0; i < sizeS; i++)
+{
+for (j = 0; j < sizeAccept; j++)
+{
+if (s[i] == accept[j])
+{
+p = &accept[j];
+break;
+}
+}
+}
+return (p);
+}
