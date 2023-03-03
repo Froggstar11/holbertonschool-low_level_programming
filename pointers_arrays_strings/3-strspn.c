522bb1eada0e35144f897f7f0562e395826eb990
miguel damasco <migueldamasco111@gmail.com>
2023-03-02 22:57:03 -0800

    3

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -9,27 +9,25 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-  unsigned int i, j, z;
-  unsigned int newPosition = 0;
-  unsigned int sizeS = strlen(s);
-  unsigned int sizeAccept = strlen(accept);
-  for (i = 0; i < sizeS; i++)
-    {
-      for (j = 0; j < sizeAccept; j++)
-	{
-	      
-		  if (s[i] == accept[j])
-		    {
-		      newPosition = 1;
-		    }
-	}
-      if (newPosition != 1)
-	{
-	z = i;
-	break;
-	}
-      newPosition = 0;
-    }
-  return (z);
+unsigned int i, j, z;
+unsigned int newPosition = 0;
+unsigned int sizeS = strlen(s);
+unsigned int sizeAccept = strlen(accept);
+for (i = 0; i < sizeS; i++)
+{
+for (j = 0; j < sizeAccept; j++)
+{	      
+if (s[i] == accept[j])
+{
+newPosition = 1;
 }
-	   
+}
+if (newPosition != 1)
+{
+z = i;
+break;
+}
+newPosition = 0;
+}
+return (z);
+}
