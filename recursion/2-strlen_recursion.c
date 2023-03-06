0166724c5fc953eb32aff63e5703135c012e02c7
miguel damasco <migueldamasco111@gmail.com>
2023-03-06 06:22:22 -0800

    2

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * @s: a pointer of char type.
+ * Return: an int.
+ */
+int _strlen_recursion(char *s)
+{
+  if (*s != '\0')
+    {
+      return ( _strlen_recursion(s+1));
+    }
+}
