70bb1e77f4bbefd5e95c823febc9a2612c9aae0c
miguel damasco <migueldamasco111@gmail.com>
2023-03-07 10:28:43 -0800

    5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -6,36 +6,40 @@
  * @n: an int.
  * Return: an int.
  */
+/**
+ * findRoot - check code.
+ * @i: an int;
+ * @j: an int;
+ * @n: an int;
+ * Return: an int.
+ */
 int findRoot(int i, int j, int n)
 {
-  if( i <= j)
-    {
-      int mid = (i + j) / 2;
-      if((mid * mid == n))
-	{
-	  return mid;
-	}
-      else if (mid * mid < n)
-	{
-	  return findRoot(mid + 1, j, n);
-	}
-      else
-	{
-	  return findRoot(i, mid / 2, n);
-	}
-    }
-  return (i);
+if( i <= j)
+{
+int mid = (i + j) / 2;
+if((mid * mid == n))
+{
+return mid;
 }
-
-
-
+else if (mid * mid < n)
+{
+return findRoot(mid + 1, j, n);
+}
+else
+{
+return findRoot(i, mid / 2, n);
+}
+}
+return (i);
+}
 
 int _sqrt_recursion(int n)
 {
-  if ( n > 0)
-  return (findRoot(0, n, n));
-  else
-    return (-1);
+if (n > 0)
+return (findRoot(0, n, n));
+else
+return (-1);
 }
 
 
