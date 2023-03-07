5650cdabfff1491a4cad605771bde1c7f253e465
miguel damasco <migueldamasco111@gmail.com>
2023-03-07 09:55:04 -0800

    5

---
+++ recursion/root.c
@@ -0,0 +1,21 @@
+#include <main.h>
+
+int root(int i, int j, int number)
+{
+  if( i <= j )
+    {
+      int mid = (i + j) / 2;
+      if ((mid * mid <= number) && ((mid + 1) * (mid + 1) > number)) {
+	return mid;
+      }
+      else if (mid * mid < number)
+	{
+	  return root(mid + 1, j, number);
+	}
+      else
+	{
+	  return root(i, j - 1, number);
+	}
+    }
+  return low;
+}
