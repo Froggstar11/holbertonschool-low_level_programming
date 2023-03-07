bb11415ed98ce19ae7d773346cbb0fce550b4e36
miguel damasco <migueldamasco111@gmail.com>
2023-03-07 10:34:29 -0800

    5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -19,15 +19,15 @@ if (i <= j)
 int mid = (i + j) / 2;
 if ((mid * mid == n))
 {
-return mid;
+return (mid);
 }
 else if (mid * mid < n)
 {
-return findRoot(mid + 1, j, n);
+return (findRoot(mid + 1, j, n));
 }
 else
 {
-return findRoot(i, mid / 2, n);
+return (findRoot(i, mid / 2, n));
 }
 }
 return (i);
