c6362d39b3327a185236546d87559225ac677f86
miguel damasco <migueldamasco111@gmail.com>
2023-03-07 09:01:07 -0800

    4

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _pow_recursion - returns the value of x raised y.
+ * @x: an int.
+ * @y: an int.
+ * Return: an int.
+ */
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+{
+return (-1);
+}
+else if (y == 0 || y == 1)
+{
+return (1);
+}
+else if (y > 1)
+{
+return (x * (_pow_recursion(x,y - 1)));
+}
+return (-1);
+}
