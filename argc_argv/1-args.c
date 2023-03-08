66b156c916927223a2464d38bbe1ac16d793c23b
miguel damasco <migueldamasco111@gmail.com>
2023-03-08 10:24:40 -0800

    1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints name.
+ * @argc: an int.
+ * @argv: a pointer of int array.
+ * Return: 0.
+ */
+int main(int argc, __attribute__ ((unused)) char *argv[])
+{
+printf("%d\n", argc);
+return (0);
+}
