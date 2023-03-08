9384ec55da6cfb0668ef8529e3b128e00e28f9a2
miguel damasco <migueldamasco111@gmail.com>
2023-03-08 14:12:46 -0800

    3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -20,19 +20,17 @@ char firstNumberS[100];
 char secondNumberS[100]; 
 firstNumberS[0] = *(argv[argc - 2]);
 secondNumberS[argc] = *(argv[argc - 1]);
-int sizeFirst = strlen(firstNumberS);
-int sizeSecond = strlen(secondNumberS);
 int i, j;
 int number1;
 int number2;
 int convert;
-for (i = 0; i < sizeFirst; i++)
+for (i = 0; i < 100; i++)
 {
 convert = (((int)firstNumberS[i]) - '0');
 number1 *= 10;
 number1 += convert;
 }
-for (j = 0; j < sizeSecond; j++)
+for (j = 0; j < 100; j++)
 {
 convert = (((int)secondNumberS[j]) - '0');
 number2 *= 10;
