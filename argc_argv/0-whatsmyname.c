a15122bfdd78c2bbba83ddf44b472437ae0a144e
miguel damasco <migueldamasco111@gmail.com>
2023-03-08 09:37:18 -0800

    0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,18 +2,12 @@
 #include <stdio.h>
 /**
  * main - prints name.
- * @arg: an int.
+ * @argc: an int.
  * @argv: a pointer of int array.
  * Return: 0.
  */
 int main(int argc, char *argv[])
 {
-  char *p = argv[0];
-  int i = argc - argc;
-  while(p++)
-    {
-      putchar(*(p + i));
-      i++;
-    }
-  return (0);
+printf("%s", argv[0]);
+return (0);
 }
