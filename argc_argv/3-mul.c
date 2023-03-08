4da3ae3bb22afe5bbdae4144afe780c1a76d6818
miguel damasco <migueldamasco111@gmail.com>
2023-03-08 10:44:41 -0800

    3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -15,8 +15,8 @@ return (1);
 }
 else
 {
-int firstNumber = *(argv + 1) + '0';
-int secondNumber = *(argv + 1) + '0';
+int firstNumber = (int)*(argv + 1);
+int secondNumber = (int)*(argv + 2);
 printf("%d", firstNumber * secondNumber);
 }
 return (0);
