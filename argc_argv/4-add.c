01978c193680a7dba1601e1b3a72411008626c20
miguel damasco <migueldamasco111@gmail.com>
2023-03-08 18:13:41 -0800

    4

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -16,19 +16,19 @@ for (i = 1; i < argc; i++)
 {
 for (j = 0; argv[i][j] != '\0'; j++)
 {
-if (argv[i][j] < 48 && argv[i][j] > 57)
-{
-printf("Error\n");
-return (1);
-}
-else
+if (argv[i][j] >= 48 && argv[i][j] <= 57)
 {
 number *= 10;
 number += argv[i][j] - 48;
 }
+ else
+{
+printf("0\n");
+return (1);
+}
+}
 sum += number;
 number = 0;
-}
 }
 printf("%d\n", sum);
 return (0);
