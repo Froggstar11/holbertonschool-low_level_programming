9031988374da38a261d99b91d5f044b3a4b2e3f1
miguel damasco <migueldamasco111@gmail.com>
2023-03-08 17:08:52 -0800

    3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -20,17 +20,21 @@ int i;
 int j;
 int number1 = 0;
 int number2 = 0;
+int mul; 
 for (i = 0; argv[1][i] != '\0'; i++)
 {
 number1 *= 10;
 number1 += argv[1][i] - 48;
+printf("numero 1: %d\n", number1); 
 }
 for (j = 0; argv[2][j] != '\0'; j++)
 {
 number2 *= 10;
 number2 += argv[2][j] - 48;
+printf("numero 2: %d\n", number2); 
 }
-printf("%d\n", number1 * number2);
+mul = number1 * number2; 
+printf("%d\n", mul);
 }
 return (0);
 }
