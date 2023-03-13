aa950f3f19d2a570a5c64d8e78e48ff1d122e834
miguel damasco <migueldamasco111@gmail.com>
2023-03-12 22:49:56 -0700

    1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strdup - check code.
+ * @str: a pointer of char type.
+ * 
+ *
+ * Return:a pointer of char type.
+ */
+char *_strduo(char *str)
+{
+int size = strlen(str);
+int i;
+char p* = (char *)malloc(sizeof(char) * size);
+if (p == NULL)
+{
+return (NULL);
+}
+else
+{
+for (i = 0; i < size; i++)
+{
+p[i] = str[i];
+}
+return (p);
+}
+}
