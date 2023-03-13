786e4f5d013ddcc7aa29d1595e811d98f588dd0f
miguel damasco <migueldamasco111@gmail.com>
2023-03-13 15:16:31 -0700

    0

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * malloc_checked - allocates memory
+ * @b: an unsigned int.
+ *
+ *
+ * Return: void.
+ */
+void *malloc_checked(unsigned int b)
+{
+  void *p = malloc(sizeof(int) * b);
+  if (p == NULL)
+    {
+      exist(98);
+    }
+  else
+    {
+      return (p);
+    }
+}
