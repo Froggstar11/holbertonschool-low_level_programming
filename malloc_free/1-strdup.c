28e5b3c731e20e82b725a2fbb8c1a3b3a6f6e69f
miguel damasco <migueldamasco111@gmail.com>
2023-03-12 23:02:04 -0700

    1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -13,12 +13,12 @@ char *_strdup(char *str)
 {
 int size = strlen(str);
 int i;
-char *p = (char *)malloc(sizeof(char) * size);
+char *p = (char *)malloc(sizeof(char) * size + 1);
 if (p == NULL)
 {
 return (NULL);
 }
-if (str == NULL)
+if (&str == NULL)
 {
 return (NULL);
 }
