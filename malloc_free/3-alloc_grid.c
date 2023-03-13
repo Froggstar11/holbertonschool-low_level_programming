f012b8286f8569af1ddc39f2a30d7a6995cfe20d
miguel damasco <migueldamasco111@gmail.com>
2023-03-13 02:33:07 -0700

    3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -23,7 +23,7 @@ int **alloc_grid(int width, int height)
 	{
 	  for (i = 0; i < width; i++)
 	    {
-	      p[i] = malloc(sizeof(int) * height);
+	      p[i] = malloc(sizeof(int) * width);
 	    }
 	  for (i = 0; i < height; i++)
 	    {
