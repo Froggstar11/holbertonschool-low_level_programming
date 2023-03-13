47fbdb6d3d3269f30a114c7497d8862fdb1af87c
miguel damasco <migueldamasco111@gmail.com>
2023-03-13 13:51:07 -0700

    4

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - free a grid of integers.
+ * @grid: the address of the two dimensional grid.
+ * @height: height of the grid.
+ *
+ * Return: void.
+ */
+void free_grid(int **grid,__attribute__ (unused) int height)
+{
+int i;
+for (i = 0; grid[i] != NULL; i++)
+{
+free(grid[i]);
+free = NULL;
+}
+free(grid);
+grid = NULL;
+}
