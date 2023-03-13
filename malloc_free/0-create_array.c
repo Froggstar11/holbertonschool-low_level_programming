92ef122c579cb21291697703e306b2fc61ac22c1
miguel damasco <migueldamasco111@gmail.com>
2023-03-12 20:59:54 -0700

    0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -11,7 +11,7 @@
  */
 char *create_array(unsigned int size, char c)
 {
-char *p = (char*)malloc((sizeof(char) * size));
+char *p = (char *)malloc(sizeof(char) * size);
 unsigned int i;
 if (size == 0)
 {
