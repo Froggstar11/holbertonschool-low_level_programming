4eb1db8a50f6452b1d2aef934f40b497277adc4c
miguel <migueldamasco111@gmail.com>
2023-06-15 06:44:02 -0700

    Exercise number 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - imprimir
+ *
+ * Return: Always 0 (return)
+ **/
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+putchar(48 + i);
+putchar('\n');
+return (0);
+}
