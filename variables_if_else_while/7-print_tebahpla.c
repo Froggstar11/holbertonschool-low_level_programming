d97daf95ee5fa3496ccd2e94bcedb4ef4a4123c8
miguel <migueldamasco111@gmail.com>
2023-06-15 06:45:52 -0700

    Exercise number 7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - imprimir
+ *
+ * Return: Always 0 (return)
+ **/
+int main(void)
+{
+int i;
+for (i = 122; i > 96; i--)
+putchar(i);
+putchar('\n');
+return (0);
+}
