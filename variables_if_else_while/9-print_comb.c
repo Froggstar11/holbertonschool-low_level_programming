f5c00304618f59511f955af285160ec9b45c3cd7
miguel <migueldamasco111@gmail.com>
2023-06-15 07:13:33 -0700

    Exercise number 9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - imprimir
+ *
+ * Return: Always 0 (return)
+ **/
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+if ( i != 9)
+{  
+putchar(i);
+}
+} 
+putchar('\n');
+return (0);
+}
