8b3b1d7902b3e04c6c55d183b2912215d1a77368
miguel <migueldamasco111@gmail.com>
2023-06-15 09:43:50 -0700

    Exercise number 0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - imprimir
+ *
+ * Return: Always 0 (return)
+ **/
+int main(void)
+{
+_putchar("_putchar\n");
+return (0); 
+}
