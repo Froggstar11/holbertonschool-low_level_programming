f2b187f658cc950eecc3b9c444d904b4fa6bfdc3
miguel <migueldamasco111@gmail.com>
2023-06-15 10:02:12 -0700

    Exercise number 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "main.h"
+/**
+ * main - imprimir
+ *
+ * Return: Always 0 (return)
+ **/
+void print_alphabet(void)
+{
+int i;
+for (i = 97; i < 123; i++)
+_putchar(i + '0');
+_putchar('\n'); 
+}
