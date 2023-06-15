63f5ca7714c92dc69ff60cf0621a7a5857069761
miguel <migueldamasco111@gmail.com>
2023-06-15 06:29:17 -0700

    Exercise number 3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - imprimir
+ *
+ * Return: Always 0 (return)
+ **/
+int main(void)
+{
+int i;
+for (i = 97; i < 123; i++)
+putchar(i);
+for (i = 65; i < 91; i++)
+putchar(i);  
+putchar('\n');
+return (0);
+}
