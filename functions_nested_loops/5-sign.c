bc39dd117ea7116b370ec9aa6a55848f77ff4e97
miguel <migueldamasco111@gmail.com>
2023-06-16 07:25:49 -0700

    Exercise number 5

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -13,7 +13,7 @@ int print_sign(int n)
 {
 if (n > 0)
 {  
-_putchar(45);
+_putchar(43);
 return (1);
 }
 if (n == 0)
@@ -23,7 +23,7 @@ return (0);
 }
 if (n < 0)
 {
-_putchar(43);
+_putchar(45);
 return (-1);
 }
 return (0);
