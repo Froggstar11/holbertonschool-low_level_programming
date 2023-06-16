0cbbb81a64560766927353895636752bb836bda1
miguel <migueldamasco111@gmail.com>
2023-06-16 06:07:57 -0700

    Exercise number 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "main.h"
+/**
+ * main - imprimir
+ *
+ * Return: Always 0 (return)
+ **/
+void print_alphabet_x10(void)
+{
+int i;
+int j;
+for (j = 0; j < 10; j++)
+{
+for (i = 97; i < 123; i++)
+{
+_putchar(i);
+_putchar('\n');
+}
+}
+}
