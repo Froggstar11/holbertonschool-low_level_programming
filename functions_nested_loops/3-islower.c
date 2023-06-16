13f650e405a87b5e3188d0816542ddbe583d8c6c
miguel <migueldamasco111@gmail.com>
2023-06-16 06:17:34 -0700

    Exercise number 3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "main.h"
+/**
+ * _islower - checks if 
+ * @c: an int.
+ * Return: 1 if c is lowercase, 0 otherwise
+ **/
+int _islower(int c)
+{
+if (c > 96 && c < 123)
+return (1);
+return (0);
+}
