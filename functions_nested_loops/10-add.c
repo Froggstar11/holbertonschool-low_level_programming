f4843a0ea11b5aeb61cb2a1bcbd9d0ee213e4b7c
miguel <migueldamasco111@gmail.com>
2023-06-16 11:19:24 -0700

    Exercise number 9

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "main.h"
+/**
+ * add - add two integers.
+ * @first: an int.
+ * @second: an int.
+ * Return: the result of the adds
+ **/
+int add(int first, int second)
+{
+return (first + second);
+}
