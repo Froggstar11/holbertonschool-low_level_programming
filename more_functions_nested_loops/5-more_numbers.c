6765047e22463a917488a6f03d96082b5833a7e3
miguel <migueldamasco111@gmail.com>
2023-06-20 08:57:42 -0700

    Exercise number 5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -14,9 +14,9 @@ for (i = 0; i < 10; i++)
 for (j = 0; j < 15; j++)
 {
 if (j > 9)
-_putchar((j / 10) + 48);  
-_putchar((j % 10) + 48); 
+_putchar((j / 10) + 48);
+_putchar((j % 10) + 48);
 }
-_putchar('\n'); 
+_putchar('\n');
 }
 }
