14cdab0d824e69f152a7dccdfd8281924de3f0d1
miguel <migueldamasco111@gmail.com>
2023-06-20 07:01:26 -0700

    Exercise number 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _isupper - checks if c is uppercase.
+ *
+ * Return: void.
+ **/
+void print_numbers(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+_putchar(i + 48);
+_putchar('\n');
+}
