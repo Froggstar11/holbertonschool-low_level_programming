4cc08d41f23be5105e214d5d655df6783e4f2992
miguel <migueldamasco111@gmail.com>
2023-06-20 18:32:40 -0700

    Exercise number 9

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -10,6 +10,10 @@ void print_triangle(int size)
 int i;
 int j;
 int cont = size - 1;
+if (size < 0)
+_putchar('\n');
+else
+{
 for (i = 0; i < size; i++)
 {
 for (j = 0; j < size; j++)
@@ -17,7 +21,9 @@ for (j = 0; j < size; j++)
 if (j == cont)
 _putchar(35);
 _putchar(32);
+cont--; 
 }
 _putchar('\n');
+}
 }
 }
