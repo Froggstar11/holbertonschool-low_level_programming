26bb4e41eada6a623914a5b04d9a403a9f6df63d
miguel <migueldamasco111@gmail.com>
2023-06-23 08:37:10 -0700

    Exercise number 1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - swap the values of two integers.
+ * @a: an int.
+ * @b: an int.
+ * Return: void.
+ */
+void swap_int(int *a, int *b)
+{
+  int valueA = *a;
+  int valueB = *b;
+
+  *a = valueB;
+  *b = valueA;
+}
