afea3996c91021c9b1ab933f9dcc39674873381b
miguel <migueldamasco111@gmail.com>
2023-06-26 10:55:16 -0700

    Exercise number 7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts_half - prints half of a string.
+ * @str: a pointer of char type.
+ * Return: void.
+ */
+void puts_half(char *str)
+{
+int i;
+int size = strlen(str);
+for (i = size - 1 / 2; s[i] != '\0'; i++)
+{
+_putchar(s[i]);      
+}
+_putchar('\0');
+}
