380e9034c68c4ad2024a85b0e3d8ff24f9688d4a
miguel <migueldamasco111@gmail.com>
2023-06-26 09:16:07 -0700

    Exercise number 5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -8,18 +8,18 @@
  */
 void rev_string(char *s)
 {
-int i;
-int aux[20];
-int count = 0;
-int size = strlen(s);
-for (i = size - 1; i >= 0; i--)
+char m;
+char *j;
+char *i;
+for (i = s; *i != '\0'; ++i)
+;
+if (i > s)
+--i;
+for (j = s; j < i; ++j, --i)
 {
-aux[count] = s[i];
-count++;
-}
-for (i = 0; aux[i] != '\0'; i++)
-{
-s[i] = aux[i];
+m = *j;
+*j = *i;
+*i = m;
 }
 }
    
