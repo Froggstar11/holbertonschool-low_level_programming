f83d6ae4623855e28219abfdfa9782a0e4fb91ca
miguel <migueldamasco111@gmail.com>
2023-06-26 09:27:12 -0700

    Exercise number 6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string.
+ * @str: a pointer of char type.
+ * Return: void.
+ */
+void puts2(char *str)
+{
+int i;
+int count = 0;
+for (i = 0; str[i] != '\0'; i++)
+{
+if (count % 2 == 0) 
+_putchar(str[i]);
+}
+_putchar('\n');
+}
