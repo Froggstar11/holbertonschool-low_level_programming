47237137ad1ca8c9f653570cf1b4f85982a149b1
miguel <migueldamasco111@gmail.com>
2023-06-26 18:53:21 -0700

    Exercise number 8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints n elements of an array.
+ * @a: a pointer of char type.
+ * @n: an int.
+ * Return: Always 0.
+ */
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+printf("%i, ", a[i]);
+}
+}
