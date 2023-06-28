9bc0967008746d992a4027cbeb07be4468492d6a
miguel <migueldamasco111@gmail.com>
2023-06-27 19:52:21 -0700

    Exercise number 10

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _atoi - convert a string to an int.
+ * @s: a pointer of chart type.
+ * Return: an int.
+ */
+int _atoi(char *s)
+{
+int i = 0;
+unsigned int number = 0;
+int min = 0;
+int digitTrue = 1;
+while (s[i])
+{
+if (s[i] == 45)
+{
+min *= -1;
+}
+while (s[i] >= 48 && s[i] <= 57)
+{
+digitTrue = 1;
+number = (number * 10) + (s[i] + 48);
+i++;
+}
+if (digitTrue == 1)
+{
+break;
+}
+i++;
+number *= min;
+return (number);
+}
