66ecfdb61f2a0705eb4710fc93e5cfd574882eee
miguel <migueldamasco111@gmail.com>
2023-07-02 16:55:56 -0700

    Exercise number 6

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,17 +1,29 @@
 #include "main.h"
 #include <stdio.h>
-#include "main.h"
-#include <stdio.h>
+
 
 /**
- * is_prime_number - find prime number.
+ * find_prime - checks if the number is a prime.
+ * @n: an int.
+ * Return: an int.
+ */
+int find_prime(int n, int y)
+{
+if (n <= 1)
+return (0);
+else if (n % y == 0 && y > 1)
+return (0);
+else if ((n / y) < y)
+return (1);
+else
+return (find_prime(n, y + 1));
+}
+/**
+ * is_prime_number - start function
  * @n: an int.
  * Return: an int.
  */
 int is_prime_number(int n)
 {
-if ((n > 1) && (n % n == 0) && (n % 1 == 0))
-return (1);
-else
-return (0);
+return (find_prime(n, 1));  
 }
