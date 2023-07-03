106c83d34a98b6db77a0f2f93f368b335fad89d2
miguel <migueldamasco111@gmail.com>
2023-07-02 19:34:06 -0700

    Exercise number 4

---
+++ argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - check the code
+ * @argc: an int.
+ * @argv: an array of char.
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+int i, j, count, result;
+if (argc == 1)
+{
+printf("0\n");
+}
+else
+{
+for (i = 0; i < argc; i++)
+{
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if(!(isdigit(argv[i][j])))
+{
+printf("Error\n");
+return (1);
+}
+}
+count = atoi(argv[i]);
+result += count;
+}
+printf("%d\n", result);
+}
+return (0);
+}
