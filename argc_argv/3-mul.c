08ea45f9225752f3d98d5a39f978966e1b66d4a7
miguel <migueldamasco111@gmail.com>
2023-07-02 18:58:56 -0700

    Exercise number 3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -11,6 +11,7 @@ int main(int argc, char *argv[])
 {
 int first = 0;
 int second = 0;
+int final = 0; 
 if (argc < 3)
 {
 printf("Error\n");
@@ -20,7 +21,8 @@ else
 {
 first = atoi(argv[1]);
 second = atoi(argv[2]);
-printf("%i\n", first * second);
+final = first * second;
+printf("%i\n", final);
 }
 return (0);
 }
