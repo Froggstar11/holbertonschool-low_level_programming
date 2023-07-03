08cfaa3323350dfb665ad21073fb3991b8802b48
miguel <migueldamasco111@gmail.com>
2023-07-02 19:10:22 -0700

    Exercise number 3

--- argc_argv/_mul.c
+++ argc_argv/_mul.c
@@ -1,12 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 
-/**
- * main - check the code
- * @argc: an int.
- * @argv: an array of char.
- * Return: Always 0.
- */
 int _mul(int a, int b)
 {
 return (a * b);
