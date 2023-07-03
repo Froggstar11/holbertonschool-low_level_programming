3dc0ef974d4001c3211ed7c3a91e51342f627ab7
miguel <migueldamasco111@gmail.com>
2023-07-02 18:56:20 -0700

    Exercise number 3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * @argc: an int.
+ * @argv: an array of char.
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+int first = 0;
+int second = 0;
+if (argc < 3)
+{
+printf("Error\n");
+return (0);
+}
+else
+{
+first = atoi(argv[1]);
+second = atoi(argv[2]);
+printf("%i\n", first * second);
+}
+return (0);
+}
