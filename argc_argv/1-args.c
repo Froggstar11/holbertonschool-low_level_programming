0d06f98e1223a2bcca64a26c04e0a18ee2b8f9f0
miguel <migueldamasco111@gmail.com>
2023-07-02 18:37:10 -0700

    Exercise number 0

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * @argc: an int.
+ * @argv: an array of char.
+ * Return: Always 0.
+ */
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+printf("%i\n", argc);
+return (0);
+}
