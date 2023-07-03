11a429309ce8f2acaf6a360d62bd4893c764569f
miguel <migueldamasco111@gmail.com>
2023-07-02 18:28:16 -0700

    Exercise number 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * @argc: an int.
+ * @argv: an array of char.
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+}
