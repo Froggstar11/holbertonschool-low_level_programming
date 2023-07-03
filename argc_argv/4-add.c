bcb9a5e1eed52a7f52d742c1ab745d2f9c59ecf8
miguel <migueldamasco111@gmail.com>
2023-07-02 19:36:31 -0700

    Exercise number 4

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -10,14 +10,14 @@
  */
 int main(int argc, char *argv[])
 {
-int i, j, count, result;
+int i, j, count, result = 0;
 if (argc == 1)
 {
 printf("0\n");
 }
 else
 {
-for (i = 0; i < argc; i++)
+for (i = 1; i < argc; i++)
 {
 for (j = 0; argv[i][j] != '\0'; j++)
 {
