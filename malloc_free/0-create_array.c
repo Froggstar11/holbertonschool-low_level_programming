716a894cbdf92bdc495a0631e9bd3dfcea322070
miguel <migueldamasco111@gmail.com>
2023-07-05 06:46:10 -0700

    Exercise 0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -10,7 +10,9 @@
 char *create_array(unsigned int size, char c)
 {
 char *p = (char *)malloc(sizeof(char) * size);
-unsigned int i; 
+unsigned int i;
+if (size == 0)
+return (0);  
 if (p == NULL)
 return (0);
 for (i = 0; i < size; i++)
