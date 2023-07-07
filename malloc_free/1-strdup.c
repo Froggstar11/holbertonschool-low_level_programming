355e8a39d26969991e2fbec2337fd28aa5bf16d6
miguel <migueldamasco111@gmail.com>
2023-07-07 05:50:02 -0700

    Exercise number 1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strdup - allocate memory for a string
+ * @str: a pointer of char type.
+ * Return: a pointer of char type.
+ */
+char *_strdup(char *str)
+{
+int size = strlen(str);
+int i;
+char *p = (char *)malloc(sizeof(char) * size));
+if (p == NULL)
+return (NULL);
+for (i = 0; i < size; i++)
+{
+p[i] = str[i];
+}
+return (p);
+}
