8eaf65a19c2b5ce49b77b0e59e47e9adb8175507
miguel <migueldamasco111@gmail.com>
2023-07-09 15:43:35 -0700

    Exercise number 0

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -9,10 +9,8 @@
  */
 void *malloc_checked(unsigned int b)
 {
-unsigned int *p = malloc(sizeof(unsigned int) * 1);
+unsigned int *p = malloc(b);
 if (p == NULL)
-printf("98");
-else
-*p = b;
+exit(98);
 return (p); 
 }
