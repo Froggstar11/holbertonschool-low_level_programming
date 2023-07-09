1bdc7b65280f7f17605a725ff288ab9e150c1fe8
miguel <migueldamasco111@gmail.com>
2023-07-08 22:12:01 -0700

    Exercise nubmer 3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -10,27 +10,27 @@
  */
 int **alloc_grid(int width, int height)
 {
-  int i, j, z;
+int i, j, z;
 int **p = NULL; 
 if (height == 0 || width == 0)
 return (NULL);
 p = (int **)malloc(sizeof(int *) * height);
 if (p == NULL)
 return (0);
- for (i = 0; i < height; i++)
-   {
-     p[i] = (int *)malloc(sizeof(int) * width);   
-     for (j = 0; j < width; j++)
-       {
-	 if (p[i] == NULL)
-	   {
-	     for (z = 0; z <= i; z++)
-	       free(p[z]);
-	     free(p);
-	   return (NULL);
-	   }
-	 p[i][j] = 0;
-       }
-   }
- return (p);
+for (i = 0; i < height; i++)
+{
+p[i] = (int *)malloc(sizeof(int) * width);   
+for (j = 0; j < width; j++)
+{
+if (p[i] == NULL)
+{
+for (z = 0; z <= i; z++)
+free(p[z]);
+free(p);
+return (NULL);
+}
+p[i][j] = 0;
+}
+}
+return (p);
 }
