2c39d83c5e72cfe542b4588672d1796e0f77b063
miguel <migueldamasco111@gmail.com>
2023-07-09 18:35:56 -0700

    Exercise number 3

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -4,9 +4,10 @@
 #include <string.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * array_range - creates an array of integers.
+ * @min: an int.
+ * @max: an int.
+ * Return: a pointer of int type or NULL if fails.
  */
 int *array_range(int min, int max)
 {
@@ -15,6 +16,8 @@ int i, count = 0;
 if (min > max)
 return (NULL);
 p = malloc(((max - min) + 1) * sizeof(int));
+if (p == NULL)
+return (NULL);
 for (i = min; i <= max; i++)
 {
 p[count] = i;
