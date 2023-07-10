8a0d7f297ea6092599476a771ee8a6d394b34ae5
miguel <migueldamasco111@gmail.com>
2023-07-09 17:43:19 -0700

    Exercise number 2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -11,11 +11,10 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 int *p = NULL;
- unsigned int i, total;
+unsigned int i;
 if (nmemb == 0 || size == 0)
-return (NULL);
-total = nmemb * size; 
-p = malloc(total);
+return (NULL); 
+p = malloc(size * nmemb);
 if (p == NULL)
 return (NULL);
 for (i = 0; i < nmemb; i++)
