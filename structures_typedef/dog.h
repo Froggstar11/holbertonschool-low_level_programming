c5d90ae21740ca2763b02e31b767bf3b80ff6c98
miguel <migueldamasco111@gmail.com>
2023-07-13 15:24:44 -0700

    Exercise number 0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOH_H
+#define DOG_H
+/**
+ * struct dog - a struct data type
+ *
+ * @name: name of the dog.
+ * @age: age of the dog.
+ * @owner: owner of the dog.
+ */
+
+struct dog
+{
+float age;
+char *name;
+char *owner;
+}
