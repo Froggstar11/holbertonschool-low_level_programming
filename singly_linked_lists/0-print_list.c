25484aed22e2261ee039240b452197e05db71e6a
miguel <migueldamasco111@gmail.com>
2023-07-30 09:58:06 -0700

    Exercise number 0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -14,7 +14,7 @@ size_t print_list(const list_t *h)
 int i;
 for (i = 0; h != NULL; i++)
 {
-if(h->str == NULL)
+if (h->str == NULL)
 printf("[0] (nil)\n");
 else
 printf("[%u] %s\n", h->len, h->str);
