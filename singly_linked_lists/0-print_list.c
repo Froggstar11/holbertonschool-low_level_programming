85a32676938c0bbae661dd972052e6dcf969b38a
miguel <migueldamasco111@gmail.com>
2023-07-30 09:53:27 -0700

    Exercise number 0

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: a pinter of struct list_t type.
+ * Return: a size_t.
+ */
+
+size_t print_list(const list_t *h)
+{
+int i;
+for (i = 0; h != NULL; i++)
+{
+if(h->str == NULL)
+printf("[0] (nil)\n");
+else
+printf("[%u] %s\n", h->len, h->str);
+h = h->next;
+}
+return (i);
+}
