c133c0cc2efa6c75893040c8f114cd502258bb42
miguel <migueldamasco111@gmail.com>
2023-07-30 20:09:31 -0700

    Exericse number 2

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -11,9 +11,9 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-unsigned long int mask;  
+unsigned long int mask;
 if (index >= sizeof(unsigned long int) * 8)
-return -1;
+return (-1);
 mask = 1UL << index;
-return (n & mask) != 0;
+return ((n & mask) != 0);
 }
