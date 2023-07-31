876ca811b16fc53b91327bad545cc95f961f3f20
miguel <migueldamasco111@gmail.com>
2023-07-30 19:24:13 -0700

    Exercise number 6

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * sum_dlistint - returns the sum of all the data (n) of a dlistint_t linked list
+ * @head: a pointer to a dobulelist node.
+ * Return: an int.
+ */
+int sum_dlistint(dlistint_t *head)
+{
+dlistint_t *position = head;
+int sum = 0;
+if (position == NULL)
+return (0);
+for (; position != NULL; )
+{
+sum += position->n;
+position = position->next;
+}
+return (sum);
+}
