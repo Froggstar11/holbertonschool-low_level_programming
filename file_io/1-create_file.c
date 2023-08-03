df0c4f40e43c2d7bfa0ce29fafd342396a20705b
miguel <migueldamasco111@gmail.com>
2023-08-03 13:48:02 -0700

    Exercise number 1

---
+++ file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * creats_file - creats a file.
+ * @filename: a pointer of char type.
+ * @text_content: a pointer of char type.
+ * Return: 1 on succes, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+ssize_t openFile, writeFile, size = 0;
+if (filename == NULL)
+return (-1);
+openFile = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+if (openFile == -1)
+return (-1);
+if (text_content != NULL)
+size = strlen(text_content);
+writeFile = write(openFile, text_content, size);
+if (writeFile == -1)
+return (-1);
+close(openFile);
+return(1);
+}
