f2e967bdf3caf705282544d3c6dc54ecaeb8191a
miguel <migueldamasco111@gmail.com>
2023-08-03 16:14:03 -0700

    Exercise number 0

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_t - creates a hast table.
+ * @size: is the size of the array.
+ * Return: a pointer to the newly created hash table.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+unsigned long int i = 0;
+hash_table_t *newTable = NULL;
+newTable = (hash_table_t *)malloc(sizeof(hash_table_t));
+if (newTable == NULL);
+return (NULL);
+newTable->array = (hash_table_t *)malloc(sizeof(hash_table_t *) * size);
+if (newTable->array == NULL)
+return (NULL);
+while (i < size)
+{
+newTable->array[i] = NULL;
+i++;
+}
+newTable->size = size;
+return (newTable);
+}
