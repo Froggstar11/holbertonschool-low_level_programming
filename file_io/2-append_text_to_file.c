a807cadb9422dfb7c724e7e5f5e9565c53748da7
miguel <migueldamasco111@gmail.com>
2023-08-03 14:03:53 -0700

    exercise number 2

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,29 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * append_text_to_file - append test at the end of a file.
+ * @filename: a pointer of char type.
+ * @text_content: a pointer of char type.
+ * Return: 1 on succes, -1 on failure.
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+ssize_t openFile, writeFile, size = 0;
+if (filename == NULL)
+return (-1);
+openFile = open(filename, O_RDWR | O_APPEND, 0600);
+if (openFile == -1)
+return (-1);
+if (test_content != NULL)
+size = strlen(text_content);
+writeFile = write(openFile, text_content, size);
+if (writeFile == -1)
+return (-1);
+close(openFile);
+return (1);
+}
