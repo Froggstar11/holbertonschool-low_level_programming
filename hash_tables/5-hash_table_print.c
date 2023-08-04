7c57196c3c4500e4ec06a0660f20f58979b28988
miguel <migueldamasco111@gmail.com>
2023-08-03 19:07:31 -0700

    Exercise number 5

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - adds an element to the hash table.
+ * @ht: hash table you want to add or update the key/value.
+ * Return: void.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+int i, boolean = 1;
+hash_node_t *auxiliar = NULL;
+if (ht != NULL)
+{
+printf("{");
+for (i = 0; i < ht->size; i++)
+{
+auxiliar = ht->attay[i];
+for (; auxiliar != NULL; )
+{
+if (boolena == 1)
+boolean = 0;
+else
+printf(", ");
+printf("'%s': '%s'", auxiliar->key, auxiliar->value);
+auxiliar = auxiliar->next;
+}
+}
+}
+printf("}\n");
+}
