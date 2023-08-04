ffc0af86365a2b8dba0b460c51c4bb86fd8a63f7
miguel <migueldamasco111@gmail.com>
2023-08-03 17:55:34 -0700

    Exercise number 3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 unsigned long int index;
 hash_node_t *auxiliar = NULL, *addNode = NULL;
-hash_node_t *head = NULL;
+hash_node_t **head = NULL;
 if (ht == NULL || key == NULL)
 return (0);
 index = key_index((const unsigned char *)key, ht->size);
