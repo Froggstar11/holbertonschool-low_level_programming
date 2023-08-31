f9f51fe63b8e6389fb01f990ceb447d7212ab1c0
miguel <migueldamasco111@gmail.com>
2023-08-30 21:36:25 -0700

    Exercise number 2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdio.h>
-
+#include <string.h>
 /**
  * _strchr - locates a character in a string.
  * @s: a pointer of char type.
@@ -10,8 +10,8 @@
  */
 char *_strchr(char *s, char c)
 {
-unsigned int i;
-for (i = 0; s[i] != '\0'; i++)
+unsigned int i, size = strlen(s) + 1; 
+for (i = 0; i < size; i++)
 {
 if (s[i] == c)
 return (&(s[i]));
